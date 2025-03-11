35985662626b85ef7f1cf93da401c148cea400a6
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-03-11 13:19:31 -0600

    add device repository test

---
+++ __tests__/devices/repository/device.repository.test.ts
@@ -0,0 +1,18 @@
+import { getDeviceTypes } from "../../../src/devices/repository/device.repository";
+import axios from "axios";
+
+jest.mock("axios");
+
+describe("getDeviceTypes", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("request fails, returns empty list", async () => {
+        (axios.get as jest.Mock).mockRejectedValue(new Error("Network Error"));
+
+        const result = await getDeviceTypes();
+
+        expect(result).toEqual([]);
+    })
+});
